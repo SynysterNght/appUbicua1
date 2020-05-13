65334253ecf2f2ba57a6a6888e6b3129c3b1a421
SynysterNght <xxpedroatxx@gmail.com>
2020-05-12 21:30:57 -0400

    First Class

---
+++ appUbicua1/appUbicua1/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using appUbicua1.Helpers;
+using appUbicua1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace appUbicua1.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Attributes
+        private ApiService apiService;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Pagos> pagos { get; set; }
+        #endregion
+
+        #region Contructor
+        public MainViewModel()
+        {
+            apiService = new ApiService();
+            pagos = new ObservableCollection<Pagos>();
+            LoadPagos();
+        }
+        #endregion
+
+        #region Metodos
+        private async void LoadPagos()
+        {
+            var rsp = await apiService.Get<Pagos>("https://",
+                                    "api/",
+                                    "Pagos");
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ appUbicua1/appUbicua1/ViewModels/PagosViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appUbicua1.ViewModels
+{
+    public class PagosViewModel
+    {
+    }
+}
